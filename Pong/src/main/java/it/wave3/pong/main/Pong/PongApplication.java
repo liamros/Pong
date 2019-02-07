20e6047325cc01df56dda2512b14ec47452a44d8
liamros <liam.rossi199@gmail.com>
2019-02-07 16:36:09 +0100

    first commit

---
+++ Pong/src/main/java/it/wave3/pong/main/Pong/PongApplication.java
@@ -0,0 +1,14 @@
+package it.wave3.pong.main.Pong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PongApplication.class, args);
+	}
+
+}
+
---
+++ Pong/src/test/java/it/wave3/pong/main/Pong/PongApplicationTests.java
@@ -0,0 +1,17 @@
+package it.wave3.pong.main.Pong;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PongApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
